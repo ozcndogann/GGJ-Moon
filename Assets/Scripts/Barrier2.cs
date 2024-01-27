4f5fd8f3c5b372a1fa4fe733a465c323c844a6ee
Can Gezer <gezer17.cg@gmail.com>
2024-01-27 08:43:42 +0300

    barriers

---
+++ Assets/Scripts/Barrier2.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier2 : MonoBehaviour
+{
+    public Transform player;
+    private bool Act = false;
+    Animator animator;
+    public bool Completed2;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.z >= 111 && !Act)
+        {
+            animator.SetBool("isOpening", true);
+            Act = true;
+        }
+
+
+        if (Completed2)
+        {
+            animator.SetBool("isClosing", true);
+        }
+    }
+}
